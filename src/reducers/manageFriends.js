e786d63af42eac36cd54e38eff5932eecb6a1b2d
techtwins <gshevaughn@gmail.com>
2021-01-25 09:42:02 -0500

    commit tree 3aa6d586c457023527aba1e3ce717b4a071a0e97

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [] }, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return { friends: [...state.friends, action.friend] };
+    case "REMOVE_FRIEND":
+      const arr = [...state.friends].filter(
+        (friend) => friend.id !== action.id
+      );
+      state = {
+        friends: arr,
+      };
+  }
+  return state;
 }
